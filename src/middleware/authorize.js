616f748db46c6d123ef791f836b0a1e0c23a3449
Chinmay jaiswal <chinmay1000@gmail.com>
2026-06-24 13:33:39 +0530

    ipdate middleware

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -1,8 +1,13 @@
 const AppError = require('../utils/AppError');
 
-const authorize = (...roles) => (req, res, next) => {
-  if (!roles.includes(req.user?.role)) return next(new AppError('Forbidden', 403));
-  next();
+// Accepts roles as arguments, e.g., authorize('ADMIN'), authorize('USER', 'STORE_OWNER')
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new AppError('You do not have permission to perform this action', 403);
+    }
+    next();
+  };
 };
 
 module.exports = authorize;
--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,12 +1,19 @@
 const AppError = require('../utils/AppError');
 
-const validate = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
-  if (error) {
-    const message = error.details.map((d) => d.message).join(', ');
-    return next(new AppError(message, 422));
-  }
-  next();
+// Middleware factory that validates request data against a Joi schema
+const validate = (schema, property = 'body') => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req[property], {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+    if (error) {
+      const message = error.details.map((d) => d.message).join(', ');
+      throw new AppError(message, 400);
+    }
+    req[property] = value; // replace with sanitized values
+    next();
+  };
 };
 
 module.exports = validate;
