364553418c9cbf8ce2f7f1b393820aa9d89f2d9b
Chinmay jaiswal <chinmay1000@gmail.com>
2026-06-24 14:30:46 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -10,11 +10,19 @@ const userRoutes = require('./features/user/user.routes');
 const storeRoutes = require('./features/store/store.routes');
 const ratingRoutes = require('./features/rating/rating.routes');
 const storeOwnerRoutes = require('./features/store-owner/store-owner.routes');
+const http = require("node:http");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+
+        origin: 'http://localhost:5173',
+        credentials: true
+
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 
